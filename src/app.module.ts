284fad4d765c334c1cf8df1e7b5372e483c800a2
sahand_zeynol <sahand_zeynol@yahoo.com>
2023-01-29 01:48:04 +0330

    add serviceBus

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServiceBusModule } from './service-bus/service-bus.module';
 
 @Module({
-  imports: [],
+  imports: [ServiceBusModule],
   controllers: [AppController],
   providers: [AppService],
 })
